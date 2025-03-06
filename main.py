a3e096b3148e03b5e17fc13e57ead047d77d8ceb
Maria Lipa <machinatororis@gmail.com>
2025-03-06 19:52:10 +0200

    added: basic operating logic

--- main.py
+++ main.py
@@ -11,17 +11,38 @@ API_KEY = os.getenv("API_KEY")
 # Проверяем, есть ли ключ
 if not API_KEY:
     print("Ошибка: API-ключ не найден. Проверьте файл .env")
+    exit()
 else:
     print("API-ключ загружен успешно!")
 
-# Тестовый запрос
-url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD"
+# Запрашиваем у пользователя данные для конвертации
+base_currency = input("Введите код исходной валюты: ").upper()
+target_currency = input("Введите код целевой валюты: ").upper()
+amount = float(input("Введите сумму для конвертации: "))
+
+# Формируем URL для запроса
+url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{base_currency}"
+
+# Делаем запрос к API
 response = requests.get(url)
 
 if response.status_code == 200:
-    print("API-ключ работает!")
     data = response.json()
-    print(f"Пример курса: 1 USD = {data['conversion_rates'] ['EUR']} EUR")
+
+    # Проверяем, успешен ли ответ
+    if data["result"] == "success":
+        print("API-ключ работает!")
+
+        # Проверяем, есть ли нужная валюта в списке
+        if target_currency in data["conversion_rates"]:
+            rate = data["conversion_rates"][target_currency]
+            converted_amount = amount * rate
+            print(f"{amount} {base_currency} = {converted_amount:.2f} {target_currency}")
+        else:
+            print("Ошибка. Валюта не найдена в базе API")
+
+    else:
+        print("Ошибка: API не смог обработать запрос. Проверьте код валюты.")
 
 else:
     print(f"Ошибка: API не отвечает. Код состояния {response.status_code}")
